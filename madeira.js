af45b072346697da153ec7521dbf3a72287c7144
Gab46512 <gabrielyamaura455@gmail.com>
2021-12-11 10:03:50 -0300

    genfer

---
+++ madeira.js
@@ -0,0 +1,11 @@
+class Tauba extends BaseClass {
+  constructor(x, y, height, angle) {
+      super(x, y, 20, height, angle)
+      this.image = loadImage("taubita.png");
+      Matter.Body.setAngle(this.body, angle);
+  }
+}
+
+
+
+  
---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+var SuperIdol;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+    //super idol
+    SuperIdol = loadImage("super idol.png");
+
+    //fileira 1
+    box1 = new Box(650,370,50,50);
+    box2 = new Box(800,370,50,50);
+    torresmin1 = new Torresmo(725,370);
+    taubate1 = new Tauba(725,350,220,PI/2);
+    //fileira 2
+    box3 = new Box(650,270,50,50);
+    box4 = new Box(800,270,50,50);
+    torresmin2 = new Torresmo(725,270);
+    taubate2 = new Tauba(725,260,220,PI/2);
+    //fileira 3
+    box5 = new Box(725,220,50,50);
+    taubate3 = new Tauba(650,160,120,PI/7);
+    taubate4 = new Tauba(790,160,120,PI/-7);
+    //chão
+    ground = new Ground(600,height,1200,20)
+    //bem te vi
+    passarro = new pessego(0, 0)
+}
+
+function draw(){    
+    background(SuperIdol);
+    Engine.update(engine);
+    //fileira1
+    box1.display();
+    box2.display();
+    torresmin1.display();
+    taubate1.display();
+    //fileira2
+    box3.display();
+    box4.display();
+    torresmin2.display();
+    taubate2.display();
+    //fileira3
+    box5.display();
+    taubate3.display();
+    taubate4.display();
+    //chin
+    ground.display();
+    //andorinha
+    passarro.display();
+    fill(255);
+    stroke(0);
+    text(Math.round(mouseX) + "," + Math.round(mouseY), mouseX, mouseY);
+}
